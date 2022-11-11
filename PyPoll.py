64b2d0fc623dc3d4460226d433b4baee658dd1f6
Paola Rivas <pfrivas@privas-MBP.attlocal.net>
2022-11-10 23:44:22 -0800

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -369,7 +369,7 @@ with open(file_to_save, "w") as txt_file:
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"---------------------------------------------\n")
+        f"--------------------------------------------\n")
 
     print(winning_candidate_summary)
 
